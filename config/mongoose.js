a631cf284d52b3aa5c5cbc32634a565e467108f2
mahesh chaudhary <maheshguru1994@gmail.com>
2022-11-08 16:32:13 +0530

    setting mongoose atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/issue_tracker_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/issue_tracker_db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(
+  "mongodb+srv://mahesh:TfdwgxN3pFuBWmCT@cluster0.tltela2.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
